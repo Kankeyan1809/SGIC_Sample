2718a9f773af7f15f13723b466c4138e0f8af911
Kiritharan Kankeyan <kankeyan2000@gmail.com>
2026-07-23 16:16:06 +0530

    r2merge

--- src/main/java/com/sgic/pratice_2/entity/School.java
+++ src/main/java/com/sgic/pratice_2/entity/School.java
@@ -16,6 +16,7 @@ public class School {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String teacher;
 
     @OneToMany(mappedBy = "school", cascade = CascadeType.ALL)
     private List<Student> students;
